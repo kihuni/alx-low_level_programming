a2fcccefa80598a5c6bdb01168dbe39ce0dff927
kihuni <stephenkihuni55@gmail.com>
2022-03-11 23:45:00 +0300

    added task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - Prints a quote
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
 	return (1);
-} 
+}
