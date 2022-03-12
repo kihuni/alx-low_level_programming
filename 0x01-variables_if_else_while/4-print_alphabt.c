9d25e61938dc1a26b5b74d7e5a40ee74135e2b22
Stephen kihuni <stephenkihuni55@gmail.com>
2022-03-12 12:13:02 +0300

    added task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - Prints the alphabet.
+ * main - Prints the alphabet without q and e.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char alp[26] = "abcdfghijklmnoprstuvwxyz";
-    int i;
+	int i;
 
-    for (i = 0; i < 52; i++)
-    {
-        putchar(alp[i]);
-    }
-    putchar('\n');
-    return (0);
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
