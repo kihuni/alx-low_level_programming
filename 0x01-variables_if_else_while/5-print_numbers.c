cd09ba3d3c70039fbf29cdf72b3ced37dbf0a2cb
Stephen kihuni <stephenkihuni55@gmail.com>
2022-03-12 14:09:47 +0300

    added task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description - prints single numbers of base 10 including 0
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
