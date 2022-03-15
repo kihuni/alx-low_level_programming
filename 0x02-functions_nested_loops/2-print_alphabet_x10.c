652b89c9405805dda85e28cfae2712648ff87968
Stephen kihuni <stephenkihuni55@gmail.com>
2022-03-15 21:39:06 +0300

    added task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,3 @@
-
-  
 #include "main.h"
 
 /**
@@ -7,22 +5,21 @@
  *
  * Return: void
  */
-
 void print_alphabet_x10(void)
 {
-	int i = 0;
-	char letter = 'a';
+    int i = 0;
 
-	while (i < 10)
-	{
-		letter = 'a';
-		while (letter <= 'z')
-		{
-			_putchar(letter);
-			letter++;
-		}
-		_putchar('\n');
+    while (i < 10)
+    {
+        char letter = 'a';
+        while (letter <= 'z')
+        {
+            _putchar(letter);
 
-		i++;
-	}
+            letter++;
+        }
+        _putchar('\n');
+
+        i++;
+    }
 }
