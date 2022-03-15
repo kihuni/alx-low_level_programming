4ebb043b17064670e86d70f40b45f1c2e91ca132
Stephen kihuni <stephenkihuni55@gmail.com>
2022-03-15 18:21:30 +0300

    added task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-       char text[9] = '_putchar\n';
-       int i;
-
-       for(i = 0; i < 8; i++)
-          {
-              _putchar(text(i))
-          }
-       _putchar("\n")
+    char text = '_putchar';
+    int i;
+    for(i=0; i<8; i++)
+    {
+        _putchar(text[i]);
+    }
+    _putchar('\');
+      
 }
 
