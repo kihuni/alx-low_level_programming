74516a4c9709c19c89b2e0114c54b1ab6856f5d7
Stephen kihuni <stephenkihuni55@gmail.com>
2022-03-17 08:41:13 +0300

    added task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,30 +7,18 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-        i = 0;
+	i = 0;
 
-       /** while (i < 10)
-        {
-                putchar(i);
-        }*/
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
 
-        printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
-
-B
-B
-A
-A
-A
-A
-A
-A
-A
-A
-A
+	return (0);
 }
