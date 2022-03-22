38f9b4ab54a36f5b8260bf076682bea20773b133
Stephen kihuni <stephenkihuni55@gmail.com>
2022-03-22 19:57:15 +0300

    added task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,37 +1,16 @@
 #include "main.h"
 
 /**
- * _atoi - convert string to integer
- * @s: the string
- * Return: int
+ * _strlen - calculates string length
+ * @s: string location pointer
+ * Return: String length
  */
 
-int _atoi(char *s)
+int _strlen(char *s)
 {
-	unsigned int counter = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	int c = 0;
 
-	while (*(s + counter) != '\0')
-	{
-		if (size > 0 && (*(s + counter) < '0' || *(s + counter) > '9'))
-			break;
-
-		if (*(s + counter) == '-')
-			pn *= -1;
-
-		if ((*(s + counter) >= '0') && (*(s + counter) <= '9'))
-		{
-			if (size > 0)
-				m *= 10;
-			size++;
-		}
-		counter++;
-	}
-
-	for (i = counter - size; i < counter; i++)
-	{
-		oi = oi + ((*(s + i) - 48) * m);
-		m /= 10;
-	}
-	return (oi * pn);
+	while (*(s + c) != '\0')
+		c++;
+	return (c);
 }
-
