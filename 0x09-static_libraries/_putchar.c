e9f181eacadc14521d54fe3d0396a9efa4f5261c
Stephen kihuni <stephenkihuni55@gmail.com>
2022-04-05 22:43:51 +0300

    Added task 0

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
+
 /**
- * main - prints _putchar
+ * main - Entry point
  *
- * Description: prints _putchar
- *
- * Return: 0 (Success)
+ * Return: nothing, return void
  */
 
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-
-	return (0);
+return (0);
 }
