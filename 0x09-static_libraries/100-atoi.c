67234dd167bc84239a41f6c8977340d350e54855
Stephen kihuni <stephenkihuni55@gmail.com>
2022-04-06 17:14:40 +0300

    added changes to task 0

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,42 +1,34 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* _atoi - converts a string to an integer.
-* @s: pointer to string.
-*
-* Return: integer gotten.
-*/
-
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
+ */
 int _atoi(char *s)
 {
-	int index, ind2;
-	unsigned int res;
-	int sign = 1;
-	char now;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	index = 0;
-	res = 0;
-	while (*(s + index) != '\0')
+	while (*(s + count) != '\0')
 	{
-		now = *(s + index);
-		if (now == '-')
-		{
-			sign *= -1;
-		}
-		if (now >= '0' && now <= '9')
-		{
-			ind2 = index;
-			while (*(s + ind2) > 47 && *(s + ind2) < 58)
-			{
-				res = (res * 10) + *(s + ind2) - '0';
-				ind2++;
-			}
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
 		}
-		index++;
+		count++;
 	}
-	if (sign < 0)
-		res *= sign;
-	return (res);
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
