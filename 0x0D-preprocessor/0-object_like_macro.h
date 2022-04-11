9e0c50e081502b6dff162085aedbde35aeeea593
Stephen kihuni <stephenkihuni55@gmail.com>
2022-04-11 19:17:14 +0300

    added task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 1024
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
+
+    s = 98 + SIZE;
+    printf("%d\n", s);
+    return (0);
+}
